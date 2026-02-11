b2210c1e72d1f139d1a663039732d743edc23f26
Hakan Güçlü <hakanguclu@proton.me>
2026-02-11 12:51:35 +0300

    implement Normal Items update to pass tests

--- app/code-refactor-frontend.ts
+++ app/code-refactor-frontend.ts
@@ -10,6 +10,23 @@ export class Item {
   }
 }
 
+const Product = {
+  NORMAL_ITEMS: "Normal Items",
+} as const;
+
+type ProductType = typeof Product[keyof typeof Product];
+type DeltaMap = { [K in ProductType]: (item: Item) => number; };
+
+const qualityDeltaMap: DeltaMap = {
+  [Product.NORMAL_ITEMS]: (item) => {
+    return item.sellIn >= 0 ? -1 : -2;
+  },
+};
+
+const sellInDeltaMap: DeltaMap = {
+  [Product.NORMAL_ITEMS]: (item) => -1,
+};
+
 export class CodeRefactorFrontEnd {
   items: Array<Item>;
 
@@ -18,8 +35,50 @@ export class CodeRefactorFrontEnd {
   }
 
   updateQuality() {
-    // TODO: Implementation goes here
+    for (let item of this.items) {
+      const itemProxy = new Proxy(item, CodeRefactorFrontEnd.itemValidator);
+      itemProxy.quality += CodeRefactorFrontEnd.getQualityDelta(item);
+      itemProxy.sellIn += CodeRefactorFrontEnd.getSellInDelta(item);
+    }
 
     return this.items;
+  }
+
+  private static itemValidator: ProxyHandler<Item> = {
+    set(target, prop, newValue) {
+      if (newValue === target[prop]) return true;
+
+      if (prop === "quality") {
+        target.quality = MathUtils.clamp(newValue, 0, 50);
+      } else {
+        target[prop] = newValue;
+      }
+
+      return true;
+    }
+  }
+
+  private static getQualityDelta(item: Item) {
+    if (item.name in qualityDeltaMap) {
+      return qualityDeltaMap[item.name](item);
+    } else {
+      return qualityDeltaMap[Product.NORMAL_ITEMS](item);
+    }
+  }
+
+  private static getSellInDelta(item: Item) {
+    if (item.name in sellInDeltaMap) {
+      return sellInDeltaMap[item.name](item);
+    } else {
+      return sellInDeltaMap[Product.NORMAL_ITEMS](item);
+    }
+  }
+}
+
+class MathUtils {
+  static clamp(value: number, min: number, max: number) {
+    if (value < min) return min;
+    if (value > max) return max;
+    return value;
   }
 }
